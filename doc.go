7bf8f5c1531738e071f907e2e044d07ba6483ee6
kalle saas <kalle@easypep.de>
2014-09-01 18:44:06 +0200

    moved read me to doc

--- doc.go
+++ doc.go
@@ -1,11 +1,54 @@
 /*
-The main schedule gives you methods to combine and build
-a schedule from workers and shifts.
-This first version implements following logic:
+Sample Go Project
+
+This library should be able to build a schedule from workers and shifts.
+
+This first version implements following simple logic:
+
 1. collect all day ranges in schedule (aka `shifts`)
-2. iterate over each shift and find an first available workers.
-Enhanced version should include more options on worker:
-Departments, WorkingHours, Rotating, Preferences, Blocking days,
-Maybe the is a more generic approach on restriction...maybe `rule sets` ?
+2. iterate over each shift and assign the first available workers.
+
+Enhanced version should include more options:
+* Departments
+* WorkingHours
+* Rotating
+* Preferences
+* Blocking days
+* ...
+
+Maybe there is a more generic approach on restriction (`rule sets`) ?
+
+We should be able to provide input as JSON:
+
+scheduler/fixtures/schedule.json
+
+
+    {
+        [
+          "starts_at" "2014-08-25T04:30:00+02:00",
+          "ends_at" "2014-08-25T08:30:00+02:00"
+        ]
+    }
+
+
+scheduler/fixtures/workers.json
+
+{
+    "id": 1,
+    "name": "kalle saas",
+    "available": [
+        {
+            "wday": 0,
+            "starts_at": 800,
+            "ends_at": 1630
+        }
+    ]
+}
+
+
+Generate local godoc with
+
+   $ gddo-server
+
 */
 package schedule
