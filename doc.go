764c5bebaf16c0bd5e9f8d542b5dff23a48d5a52
kalle saas <kalle@easypep.de>
2014-09-01 18:55:25 +0200

    doc

--- doc.go
+++ doc.go
@@ -6,14 +6,21 @@ This library should be able to build a schedule from workers and shifts.
 This first version implements following simple logic:
 
 1. collect all day ranges in schedule (aka `shifts`)
+
 2. iterate over each shift and assign the first available workers.
 
 Enhanced version should include more options:
+
 * Departments
+
 * WorkingHours
+
 * Rotating
+
 * Preferences
+
 * Blocking days
+
 * ...
 
 Maybe there is a more generic approach on restriction (`rule sets`) ?
